c334f975eae0b226a306243f6f800de934dd7860
kevios12 <kerschbaumer.kevin2@outlook.de>
2023-03-23 22:58:56 +0100

    Fixes and Cleanup
    * loader/console: DOMException: Node.removeChild: The node to be removed is not a child of this node - fixed with: replacing 'document.body.removeChild(loader);' with
    'while (loader.hasChildNodes()) {
        loader.removeChild(loader.firstChild);
    }'
    * fix media query for Desktop & Mobile
    * layout bugs fixed
    * removed un-used / shrinked Code (scss)

--- src/js/main.js
+++ src/js/main.js
@@ -25,6 +25,8 @@ window.addEventListener("load", () => {
   const loader = document.querySelector(".loader");
   loader.classList.add("loader--hidden");
   loader.addEventListener("transitionend", () => {
-    document.body.removeChild(loader);
+    while (loader.hasChildNodes()) {
+      loader.removeChild(loader.firstChild);
+    }
   });
 });
